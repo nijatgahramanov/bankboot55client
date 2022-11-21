08645a8602fc00ba6d2f383a4f6bc82b9d104360
Nijat Gahramanov <nejatqahramanov7@gmail.com>
2022-11-21 22:53:15 +0300

    Authorization info was added to header

--- src/main/java/az/orient/bankboot55client/util/Utility.java
+++ src/main/java/az/orient/bankboot55client/util/Utility.java
@@ -1,5 +1,6 @@
 package az.orient.bankboot55client.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -7,13 +8,24 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Base64;
+
 @Component
 public class Utility {
+
+    @Value("api.usr")
+    String apiUsr;
+
+    @Value("api.pwd")
+    String apiPwd;
 
     public HttpHeaders getHeaders() {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Content-type", "application/json");
         httpHeaders.add("Accept", "application/json");
+        String auth = apiUsr + ":" +apiPwd;
+        String base64Creds = Base64.getEncoder().encodeToString(auth.getBytes());
+        httpHeaders.add("Authorization","Basic "+ base64Creds);
         return httpHeaders;
     }
 
